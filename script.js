de434be0453efc4b98f0ea5462234aad92f596bd
abisri1020 <89251671+abisri1020@users.noreply.github.com>
2022-03-07 00:34:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var timer = document.getElementById("start");
+setTimeout(() => {
+    timer.innerHTML = "💣 10 ";
+    setTimeout(() => {
+        timer.innerHTML = " 9 💣";
+        setTimeout(() => {
+            timer.innerHTML = "💣 8 ";
+            setTimeout(() => {
+                timer.innerHTML = " 7 💣";
+                setTimeout(() => {
+                    timer.innerHTML = "💣 6 ";
+                    setTimeout(() => {
+                        timer.innerHTML = " 5 💣";
+                        setTimeout(() => {
+                            timer.innerHTML = "💣 4";
+                            setTimeout(() => {
+                                timer.innerHTML = " 3 💣";
+                                setTimeout(() => {
+                                    timer.innerHTML = "💣 2 ";
+                                    setTimeout(() => {
+                                        timer.innerHTML = " 1 💣";
+                                        setTimeout(() => {
+                                            timer.innerHTML = "🎉🎊 Happy Independance Day 🎊🎉";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+});
